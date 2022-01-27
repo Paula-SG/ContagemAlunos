09adccdb51518c1d529c1361fa823cebcba96436
Paula Santana Galvão <psantgalvao@gmail.com>
2022-01-27 19:24:03 -0300

    Contagem finalizada

---
+++ contagem.js
@@ -0,0 +1,8 @@
+let numeroalunos = 10
+
+for(let contador = 0; contador < numeroalunos; contador++) {  
+    if(contador == 0) {console.log ("O número é ZERO")}
+  else  if(contador % 2 == 0) {console.log (`O número ${contador} é par`)}
+else if(contador % 2 == 1) {console.log (`O número ${contador} é ímpar`)}
+
+}
